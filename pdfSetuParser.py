ebeb9287b8630f2acc0b34482bc36e965a327a8b
Jarret Jheng Ch'ng <jarretchng02@gmail.com>
2019-12-05 15:02:39 +1100

    Add agree rate method
    Made the agree rate parser into a method; useful for calling.

--- pdfSetuParser.py
+++ pdfSetuParser.py
@@ -13,21 +13,26 @@ Date: 4/12/2019
 import pdfScraper
 import re
 
-file = pdfScraper.extract_text_from_pdf("sampleSetuFile1.pdf")
+def extractAgreeRates(setuFile):
+	file = pdfScraper.extract_text_from_pdf(setuFile)
 
-# semiParsed is a list of tables
-# "\d\. " is a regex that finds a digit then a full stop then a space
-# e.g. "100. " would be accepted by the regex
-# we split it by this regex to find each table
-semiParsed = re.split("\d\. ",file)
+	# semiParsed is a list of tables
+	# "\d\. " is a regex that finds a digit then a full stop then a space
+	# e.g. "100. " would be accepted by the regex
+	# we split it by this regex to find each table
+	semiParsed = re.split("\d\. ",file)
 
-# a list to hold all of the percentages from each table
-agreeRate = []
+	# a list to hold all of the percentages from each table
+	agreeRate = []
 
-# go through each of the tables and extract the Strongly Agree/Agree part of the table
-# first item is ignored because that is the very first table that is split by "1. "
-for i in semiParsed[1:]:
-	# Split by the keyword and by the % so that we can extract the numeric value of it
-	agreeRate.append(float(i.split("Strongly Agree/Agree:")[-1].split("%")[0]))
+	# go through each of the tables and extract the Strongly Agree/Agree part of the table
+	# first item is ignored because that is the very first table that is split by "1. "
+	for i in semiParsed[1:]:
+		# Split by the keyword and by the % so that we can extract the numeric value of it
+		agreeRate.append(float(i.split("Strongly Agree/Agree:")[-1].split("%")[0]))
 
-print(agreeRate)
+	return agreeRate
+
+if __name__ == '__main__':
+	a = extractAgreeRates("sampleSetuFile1.pdf")
+	print(a)

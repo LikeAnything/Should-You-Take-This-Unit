0d832805ae5083d87f63b558a5b0a966882e51f6
Frank Lu <franklu.99@outlook.com>
2019-12-05 16:56:56 +1100

    Added chart.js, which home.html references.

---
+++ js/chart.js
@@ -0,0 +1,9 @@
+// Should draw stuff later!
+// Just code to set up the svg for now.
+const svg = d3.select("body")
+              .append("svg")
+              .attr("height", "100%")
+              .attr("width", "100%"),
+      height = 500,
+      width = 500,
+      margin = {left: 50, right: 50, top: 40, bottom: 0}

ff5949ca8074d15ff966b0fe975487e18a0848c1
Frank Lu <franklu.99@outlook.com>
2019-12-05 23:18:44 +1100

    Home.html now draws a circle that changes colour.

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,17 @@
-// Should draw stuff later!
-// Just code to set up the svg for now.
+// Setting up the svg
 const svg = d3.select("body")
               .append("svg")
               .attr("height", "100%")
               .attr("width", "100%"),
-      height = 500,
-      width = 500,
-      margin = {left: 50, right: 50, top: 40, bottom: 0}
+      // get height and width automatically
+      width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,
+      height = window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,
+      // margin = {left: 50, right: 50, top: 40, bottom: 0},
+      chartGroup = svg.append("g").attr("transform","translate("+width/2+","+height/2+")");
+
+// add a giant circle in the middle
+chartGroup.append("circle")
+          .attr("class", "back")
+          .attr("r", height/3)  // roughly 1/3 size
+          // .attr("cy", height/2)
+          // .attr("cx", width/2)

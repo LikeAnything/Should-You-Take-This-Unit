44f5eb53a56b8ebdb80ef2051aaf820d17857d1e
Frank Lu <franklu.99@outlook.com>
2019-12-06 17:37:33 +1100

    Now draws some points, and a pentagon.

--- js/chart.js
+++ js/chart.js
@@ -7,19 +7,43 @@ const svg = d3.select("body")
       width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,
       height = window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,
       // margin = {left: 50, right: 50, top: 40, bottom: 0},
-      chartGroup = svg.append("g").attr("transform","translate("+width/2+","+height/2+")");
+      chartGroup = svg.append("g").attr("class", "chart").attr("transform","translate("+width/2+","+height/2+")");
 
 // add a giant circle in the middle
+const circleRadius = height/3;
 chartGroup.append("circle")
           .attr("class", "back")
-          .attr("r", height/3)  // roughly 1/3 size
+          .attr("r", circleRadius)  // roughly 1/3 size
           // .attr("cy", height/2)
           // .attr("cx", width/2)
 
-// add text to top
-const padding = 10;
-chartGroup.append("text")
-          .attr("class", "label")
-          .attr("y", -(height/3) - padding)
-          .attr("x", 0)
-          .text("Assessment");
+const padding = (height / 50),
+      points = [
+        {x: circleRadius * Math.cos(Math.PI/2), y: -circleRadius * Math.sin(Math.PI/2), text: "Assessment"},
+        {x: circleRadius * Math.cos(21 * Math.PI/10), y: -circleRadius * Math.sin(21 * Math.PI/10), text: "Assessment"},
+        {x: circleRadius * Math.cos(17 * Math.PI/10), y: -circleRadius * Math.sin(17* Math.PI/10), text: "Assessment"},
+        {x: circleRadius * Math.cos(13 * Math.PI/10), y: -circleRadius * Math.sin(13* Math.PI/10), text: "Assessment"},
+        {x: circleRadius * Math.cos(9 * Math.PI/10), y: -circleRadius * Math.sin(9* Math.PI/10), text: "Assessment"}
+               ],
+      pentagonGroup = svg.append("g").attr("class", "pentagon");
+
+// add a small circle to each point
+points.forEach(p => {
+  chartGroup.append("circle")
+            .attr("class", "point")
+            .attr("cx", p.x)
+            .attr("cy", p.y)
+            .attr("r", 2)
+            // .text("Assessment");
+});
+
+// make a line generator
+const line = d3.line()
+               .x(d => d.x)
+               .y(d => d.y)
+               .curve(d3.curveCardinalClosed.tension(0.3));
+
+// add a path using the line generator
+chartGroup.append("path")
+          .attr("fill", "lightgray")
+          .attr("d", line(points))

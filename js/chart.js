48ba3280e2d13c50233ce62d251918f9279d562f
Frank Lu <franklu.99@outlook.com>
2019-12-08 23:25:19 +1100

    Added axis to chart. Reformatted unit scores.

--- js/chart.js
+++ js/chart.js
@@ -44,11 +44,11 @@ function queryUnit() {
 
 function getUnitScores(unitCode) {
       return {
-            assessment: (height/3) * (4/5),
-            feedback: (height/3) * (3/5),
-            satisfaction: (height/3) * (2/5),
-            resources: (height/3) * (4/5),
-            activities: (height/3) * (5/5)
+            assessment: (4/5),
+            feedback: (3/5),
+            satisfaction:  (2/5),
+            resources: (4/5),
+            activities: (5/5)
       }
 }
 
@@ -61,18 +61,26 @@ function visualise(unitCode) {
       svg
       .append("svg")
       .attr("height", "100%")
-      .attr("width", "100%"),
+      .attr("width", "100%");
+
       // get height and width automatically
-      width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,
+      const width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth,
       height = window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,
       // margin = {left: 50, right: 50, top: 40, bottom: 0},
 
-      unitScores = getUnitScores(unitCode);
+      unitScores = getUnitScores(unitCode)
+      chartGroup = svg.append("g").attr("class", "chart").attr("transform","translate("+width/2+","+height/2+")"),
+      maxRadius = height/3;
 
+      // points for each criteria
+      points = [
+            {x: unitScores.assessment * Math.cos(Math.PI/2), y: - unitScores.assessment * Math.sin(Math.PI/2), text: "Assessment"},
+            {x: unitScores.feedback * Math.cos(21 * Math.PI/10), y: -unitScores.feedback * Math.sin(21 * Math.PI/10), text: "Feedback"},
+            {x: unitScores.satisfaction * Math.cos(17 * Math.PI/10), y: -unitScores.satisfaction  * Math.sin(17* Math.PI/10), text: "Satisfaction"},
+            {x: unitScores.resources * Math.cos(13 * Math.PI/10), y: -unitScores.resources * Math.sin(13* Math.PI/10), text: "Resources"},
+            {x: unitScores.activities * Math.cos(9 * Math.PI/10), y: -unitScores.activities * Math.sin(9* Math.PI/10), text: "Activities"}
+      ].map(p => {p.x *= maxRadius; p.y *= maxRadius; return p});
 
-      chartGroup = svg.append("g").attr("class", "chart").attr("transform","translate("+width/2+","+height/2+")");
-
-      const maxRadius = height/3;
       // add 5 concentric circles
       for (i = 0; i < 5; i++) {
             const circleRadius = maxRadius - (height/15)*i;
@@ -80,17 +88,8 @@ function visualise(unitCode) {
                   .attr("class", "back")
                   .attr("stroke-opacity", String(1 - 0.2*i))
                   .attr("r", circleRadius) 
-                  // .attr("cy", height/2)
-                  // .attr("cx", width/2)
       }
 
-      points = [
-            {x: unitScores.assessment * Math.cos(Math.PI/2), y: - unitScores.assessment * Math.sin(Math.PI/2), text: "Assessment"},
-            {x: unitScores.feedback * Math.cos(21 * Math.PI/10), y: -unitScores.feedback * Math.sin(21 * Math.PI/10), text: "Feedback"},
-            {x: unitScores.satisfaction * Math.cos(17 * Math.PI/10), y: -unitScores.satisfaction  * Math.sin(17* Math.PI/10), text: "Satisfaction"},
-            {x: unitScores.resources * Math.cos(13 * Math.PI/10), y: -unitScores.resources * Math.sin(13* Math.PI/10), text: "Resources"},
-            {x: unitScores.activities * Math.cos(9 * Math.PI/10), y: -unitScores.activities * Math.sin(9* Math.PI/10), text: "Activities"}
-                  ],
 
       // add a small circle to each point
       points.forEach(p => {
@@ -99,9 +98,23 @@ function visualise(unitCode) {
                         .attr("cx", p.x)
                         .attr("cy", p.y)
                         .attr("r", 2)
-                        // .text("Assessment");
             });
-      
+
+      // creating axes
+      const axisGroup = chartGroup.append("g").attr("class", "axis"),
+            linearScale = d3.scaleLinear()
+                            .domain([0, 100])
+                            .range([maxRadius, 0]);
+      for (i = 0; i < 5; i++) {
+            // make a new group for each axis
+            let group = axisGroup.append("g")
+                     .attr("class", "axis a"+i)
+                     .attr("transform","rotate("+ i * 72 +") " +"translate(0,-"+maxRadius+")")
+            let axis = d3.axisLeft(linearScale);
+            group.call(axis)
+      }
+
+
       // make a line generator
       const line = d3.line()
                   .x(d => d.x)
